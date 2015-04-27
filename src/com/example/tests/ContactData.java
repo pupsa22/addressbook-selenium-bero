49f0b6bd0bf679b20badb4997f46b33c94902653
Oksana <ksundelb@gmail.com>
2015-04-27 20:09:50 +0300

    Removing of duplicate ContactData constructor

--- src/com/example/tests/ContactData.java
+++ src/com/example/tests/ContactData.java
@@ -18,24 +18,4 @@ public class ContactData {
 
 	public ContactData() {
 	}
-	public ContactData(String first_name, String last_name,
-			String address_1, String home_num_1, String mobile_num,
-			String work_num, String mail_1, String mail_2, String b_day,
-			String b_month, String b_year, String group, String address_2,
-			String home_num_2) {
-		this.firstName = first_name;
-		this.lastName = last_name;
-		this.address_1 = address_1;
-		this.homeNum_1 = home_num_1;
-		this.mobileNum = mobile_num;
-		this.workNum = work_num;
-		this.mail_1 = mail_1;
-		this.mail_2 = mail_2;
-		this.birthDay = b_day;
-		this.birthMonth = b_month;
-		this.birthYear = b_year;
-		this.group = group;
-		this.address_2 = address_2;
-		this.homeNum_2 = home_num_2;
-	}
 }
